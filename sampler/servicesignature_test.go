f5da7ddc712680b7bfd9468afc65f20cf5ba45e9
Christian Mauduit (DataDog) <christian.mauduit@datadoghq.com>
2017-09-04 17:36:12 +0200

    [distributed sampling] added test for service signature

--- sampler/servicesignature_test.go
+++ sampler/servicesignature_test.go
@@ -7,10 +7,10 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCombinedSignatureSimilar(t *testing.T) {
+func TestServiceSignatureSimilar(t *testing.T) {
 	assert := assert.New(t)
 
-	computer := &combinedSignatureComputer{}
+	computer := &serviceSignatureComputer{}
 
 	t1 := model.Trace{
 		model.Span{TraceID: 101, SpanID: 1011, Service: "x1", Name: "y1", Resource: "z1", Duration: 26965},
@@ -19,17 +19,17 @@ func TestCombinedSignatureSimilar(t *testing.T) {
 		model.Span{TraceID: 101, SpanID: 1014, ParentID: 1013, Service: "x2", Name: "y2", Resource: "z2", Duration: 34384993},
 	}
 	t2 := model.Trace{
-		model.Span{TraceID: 102, SpanID: 1021, Service: "x1", Name: "y1", Resource: "z1", Duration: 992312},
-		model.Span{TraceID: 102, SpanID: 1022, ParentID: 1021, Service: "x1", Name: "y1", Resource: "z1", Duration: 34347},
+		model.Span{TraceID: 102, SpanID: 1021, Service: "x1", Name: "y2", Resource: "z2", Duration: 992312},
+		model.Span{TraceID: 102, SpanID: 1022, ParentID: 1021, Service: "x1", Name: "y1", Resource: "z1", Error: 1, Duration: 34347},
 		model.Span{TraceID: 102, SpanID: 1023, ParentID: 1022, Service: "x2", Name: "y2", Resource: "z2", Duration: 349944},
 	}
 	assert.Equal(computer.ComputeSignature(t1), computer.ComputeSignature(t2))
 }
 
-func TestCombinedSignatureDifferentError(t *testing.T) {
+func TestServiceSignatureDifferentService(t *testing.T) {
 	assert := assert.New(t)
 
-	computer := &combinedSignatureComputer{}
+	computer := &serviceSignatureComputer{}
 
 	t1 := model.Trace{
 		model.Span{TraceID: 101, SpanID: 1011, Service: "x1", Name: "y1", Resource: "z1", Duration: 26965},
@@ -38,29 +38,29 @@ func TestCombinedSignatureDifferentError(t *testing.T) {
 		model.Span{TraceID: 101, SpanID: 1014, ParentID: 1013, Service: "x2", Name: "y2", Resource: "z2", Duration: 34384993},
 	}
 	t2 := model.Trace{
-		model.Span{TraceID: 110, SpanID: 1101, Service: "x1", Name: "y1", Resource: "z1", Duration: 992312},
-		model.Span{TraceID: 110, SpanID: 1102, ParentID: 1101, Service: "x1", Name: "y1", Resource: "z1", Error: 1, Duration: 34347},
-		model.Span{TraceID: 110, SpanID: 1103, ParentID: 1101, Service: "x2", Name: "y2", Resource: "z2", Duration: 349944},
+		model.Span{TraceID: 103, SpanID: 1031, Service: "x2", Name: "y1", Resource: "z1", Duration: 19207},
+		model.Span{TraceID: 103, SpanID: 1032, ParentID: 1031, Service: "x1", Name: "y1", Resource: "z1", Duration: 234923874},
+		model.Span{TraceID: 103, SpanID: 1033, ParentID: 1032, Service: "x1", Name: "y1", Resource: "z1", Duration: 152342344},
 	}
 
 	assert.NotEqual(computer.ComputeSignature(t1), computer.ComputeSignature(t2))
 }
 
-func TestCombinedSignatureDifferentRoot(t *testing.T) {
+func TestServiceSignatureDifferentEnv(t *testing.T) {
 	assert := assert.New(t)
 
-	computer := &combinedSignatureComputer{}
+	computer := &serviceSignatureComputer{}
 
 	t1 := model.Trace{
-		model.Span{TraceID: 101, SpanID: 1011, Service: "x1", Name: "y1", Resource: "z1", Duration: 26965},
+		model.Span{TraceID: 101, SpanID: 1011, Service: "x1", Name: "y1", Resource: "z1", Duration: 26965, Meta: map[string]string{"env": "test"}},
 		model.Span{TraceID: 101, SpanID: 1012, ParentID: 1011, Service: "x1", Name: "y1", Resource: "z1", Duration: 197884},
 		model.Span{TraceID: 101, SpanID: 1013, ParentID: 1012, Service: "x1", Name: "y1", Resource: "z1", Duration: 12304982304},
 		model.Span{TraceID: 101, SpanID: 1014, ParentID: 1013, Service: "x2", Name: "y2", Resource: "z2", Duration: 34384993},
 	}
 	t2 := model.Trace{
-		model.Span{TraceID: 103, SpanID: 1031, Service: "x1", Name: "y1", Resource: "z2", Duration: 19207},
-		model.Span{TraceID: 103, SpanID: 1032, ParentID: 1031, Service: "x1", Name: "y1", Resource: "z1", Duration: 234923874},
-		model.Span{TraceID: 103, SpanID: 1033, ParentID: 1032, Service: "x1", Name: "y1", Resource: "z1", Duration: 152342344},
+		model.Span{TraceID: 110, SpanID: 1101, Service: "x1", Name: "y1", Resource: "z1", Duration: 992312, Meta: map[string]string{"env": "prod"}},
+		model.Span{TraceID: 110, SpanID: 1102, ParentID: 1101, Service: "x1", Name: "y1", Resource: "z1", Duration: 34347},
+		model.Span{TraceID: 110, SpanID: 1103, ParentID: 1101, Service: "x2", Name: "y2", Resource: "z2", Duration: 349944},
 	}
 
 	assert.NotEqual(computer.ComputeSignature(t1), computer.ComputeSignature(t2))
